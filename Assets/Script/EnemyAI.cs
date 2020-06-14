fd2432286d8edae9c3a26332caceb41c8e980c7f
CJdaSuperman <chrisbel97@gmail.com>
2020-06-13 21:12:45 -0400

    Init Commit: Created a Prototype sandbox scene and Enemy AI

---
+++ Assets/Script/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform Target;
+    [SerializeField] float ChaseRange = 5f;
+
+    NavMeshAgent Agent;
+    float DistanceToTarget = Mathf.Infinity;    //avoids chasing after the player right away
+    
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        DistanceToTarget = Vector3.Distance(Target.position, transform.position);
+
+        if(DistanceToTarget <= ChaseRange)
+            Agent.SetDestination(Target.position);
+
+    }
+}
