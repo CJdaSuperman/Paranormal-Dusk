5b16553272fd8645401e162a16a44c491569783f
CJdaSuperman <chrisbel97@gmail.com>
2020-08-01 21:59:43 -0400

    Added a pond and made enemy death particles and its functionality

---
+++ Assets/Scripts/Enemy Scripts/DeathParticlesHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathParticlesHandler : MonoBehaviour
+{
+    [SerializeField] float replenishAmount = 20f;
+
+    void OnTriggerEnter(Collider other)
+    {        
+        if(other.tag == "Player")
+        {
+            PlayerHealthHandler player = other.gameObject.GetComponent<PlayerHealthHandler>();
+
+            player.AddToCurrentHealth(replenishAmount);
+            player.ShowPlayerHealthCanvas();
+
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerHealthHandler.cs
+++ Assets/Scripts/PlayerHealthHandler.cs
@@ -10,6 +10,13 @@ public class PlayerHealthHandler : MonoBehaviour
     [SerializeField] Canvas damageIndicatorCanvas;
     [SerializeField] TextMeshProUGUI playerHealthText;
 
+    float maxHealth;
+
+    void Start()
+    {
+        maxHealth = playerHealth;
+    }
+
     public void DecreaseHealth(float damage)
     {
         playerHealth -= damage;
@@ -26,8 +33,25 @@ public class PlayerHealthHandler : MonoBehaviour
     {        
         if(Input.GetKeyDown(KeyCode.CapsLock))
         {
+            ShowPlayerHealthCanvas();
+        }
+    }
+
+    public void ShowPlayerHealthCanvas()
+    {
             playerHealthText.text = playerHealth.ToString();
             playerHealthCanvas.GetComponent<PlayerHealthCanvasHandler>().FadeCanvas();
-        }
+    }
+    
+    public float GetMaxHealth() { return maxHealth; }
+
+    public float GetCurrentHealth() { return playerHealth; }  
+    
+    public void AddToCurrentHealth(float replenishedHealth) 
+    {
+        if ((playerHealth += replenishedHealth) > maxHealth)
+            playerHealth = maxHealth;
+        else
+            playerHealth += replenishedHealth;
     }
 }
